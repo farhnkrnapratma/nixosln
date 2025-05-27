b6394735d87c39f4d772305c706e6cf38953bf57
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-05-27 21:51:37 +0700

    Patch: Update wezterm config

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -4,8 +4,15 @@ return {
   font = wezterm.font 'FiraCode Nerd Font',
   font_size = 11,
   line_height = 1.2,
-  window_background_opacity = 0.7,
-  color_scheme = 'Catppuccin Latte',
+  window_decorations = "NONE",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  window_background_opacity = 0.6,
+  color_scheme = 'Catppuccin Macchiato',
   enable_tab_bar = false,
   keys = {
     {
